4203c5ef611ca27c3e474216c9abf058a9695d08
masashi <>
2023-01-07 00:39:35 +0900

    動的インポートでlocalStorage.getItemを対策済

--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -1,7 +1,12 @@
 /** @format */
 
 import Layout from "../components/Layout";
-import TodoApp from "../components/TodoApp";
+// import TodoApp from "../components/TodoApp";
+import dynamic from "next/dynamic";
+const TodoApp = dynamic(() => import("../components/TodoApp"), {
+  //サーバーサイド側でインポートはしない
+  ssr: false,
+});
 export default function Contact() {
   return (
     <Layout title="Contact">
