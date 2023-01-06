a2045abbc0890d87a307f0bcb09f6df3e9e3d6f7
masashi <>
2023-01-07 00:57:59 +0900

    jsに一旦戻す（ビルド対策）

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,7 +2,7 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
 import { client } from "../libs/client";
-import type { Blog, Tag } from "types/blog";
+// import type { Blog, Tag } from "types/blog";
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async () => {
   const data = await client.get({ endpoint: "blog" });
@@ -18,7 +18,7 @@ export default function Blog({ blog }) {
   return (
     <Layout title="Blog">
       <ul>
-        {blog.map((blog: Blog) => (
+        {blog.map((blog) => (
           <li className="Blog__box" key={blog.id}>
             <Link className="Blog__link" href={`/blog/${blog.id}`}>
               <div className="Blog__data">{blog.publishedAt}:</div>
