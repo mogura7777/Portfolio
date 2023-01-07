ca04d47029e8ca305b1a18484bbfb7cf13593714
masashi <>
2023-01-07 21:56:08 +0900

    プログレスを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,17 @@ import "styles/globals.scss";
 // import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import NextNprogress from "nextjs-progressbar";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
+      <NextNprogress
+        color="#efefef"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={2}
+        showOnShallow={true}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
