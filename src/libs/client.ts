f465c4e5aafe8ee60b95696115b7959544d650da
masashi <>
2023-01-15 21:51:25 +0900

    no message

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -5,6 +5,6 @@
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
-  serviceDomain: process.env.SERVICE_DOMAIN || "",
-  apiKey: process.env.API_KEY || "",
+  serviceDomain: process.env.SERVICE_DOMAIN || "kawamata",
+  apiKey: process.env.API_KEY || "4J3WzbXypCvCVWrkrnw3OfTfE3wZQKYQowLI",
 });
