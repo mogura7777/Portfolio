6a0faa303627ebdbad5cef5220fa2eee54674ac3
masashi <>
2023-01-28 00:46:50 +0900

    vercelの設定で解決済み

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -5,6 +5,6 @@
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
-  serviceDomain: process.env.SERVICE_DOMAIN || "kawamata",
+  serviceDomain: process.env.SERVICE_DOMAIN as string,
   apiKey: process.env.API_KEY as string,
 });
