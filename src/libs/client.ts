abfa96ff63753cb59a9ed7ca3319f41b1b35d3d0
masashi <>
2023-01-27 23:48:08 +0900

    テスト

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -5,6 +5,6 @@
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
-  serviceDomain: process.env.SERVICE_DOMAIN as string,
-  apiKey: process.env.FIREBASE_API_KEY as string,
+  serviceDomain: process.env.SERVICE_DOMAIN || "kawamata",
+  apiKey: process.env.API_KEY || "4J3WzbXypCvCVWrkrnw3OfTfE3wZQKYQowLI",
 });
